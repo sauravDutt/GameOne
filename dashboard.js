06df9ffadf5817bd9e2a9e69ff3b5db09d9625e4
sauravDutt <sauravd069@gmail.com>
2023-06-18 20:32:39 +0530

    Just added some more stuff

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,13 @@
 let canvas;
 let ctx;
 
+let mapCanvas;
+
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     ctx = canvas.getContext('2d');
+    mapCanvas = document.getElementById('mapCanvas');
+    mapCtx = mapCanvas.getContext('2d');
     canvas.window = window.innerWidth;
     canvas.height = window.innerHeight;
 }
